f7cf39f2d5cca82b591e23a9e1e970a3adea3468
ahadhossainaiman <ahadhossainaiman017@gmail.com>
2023-10-02 11:35:20 +0600

    3 level dropdown implement

--- src/components/RenderSubCategory.jsx
+++ src/components/RenderSubCategory.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/prop-types */
 
 
-const RenderSubCategory = () => {
+const RenderSubCategory = ({subCate}) => {
+    console.log(subCate);
     return (
         <div>
-            
+            <p1>{subCate.name}</p1>
         </div>
     );
 };
--- src/components/SideDropdownMenu.jsx
+++ src/components/SideDropdownMenu.jsx
@@ -1,59 +1,66 @@
 /* eslint-disable react/prop-types */
-import  { useState } from 'react';
-import RenderSubCategory from './renderSubCategory';
+import { useState } from "react";
+// import RenderSubCategory from "./renderSubCategory";
 
-const SideDropdownMenu = ({categories}) => {
-    console.log(categories);
-    // const [expandedCategories, setExpandedCategories] = useState([]);
-    const [openCategory,setOpenCategory] = useState(null);
-
+const SideDropdownMenu = ({ categories }) => {
+  console.log(categories);
+  // const [expandedCategories, setExpandedCategories] = useState([]);
+  const [openCategories, setOpenCategories] = useState([]);
+  // const [openCategory, setOpenCategory] = useState(null);
+ 
 
   const toggleCategory = (categoryId) => {
-    if(openCategory === categoryId){
-    // if (expandedCategories.includes(categoryId)) {
-    //   setExpandedCategories(expandedCategories.filter((id) => id !== categoryId));
-    // } else {
-    //   setExpandedCategories([...expandedCategories, categoryId]);
-    // }
-    setOpenCategory(null);
-  }else {
-    setOpenCategory(categoryId)
-  }
-}
+    
+    if (openCategories.includes(categoryId)) {
+      setOpenCategories(openCategories.filter((id) => id !== categoryId));
+    } else {
+      setOpenCategories([...openCategories, categoryId]);
+    }
+  };
+    
+  
+
+  
+
+
+  
 
   const renderCategory = (category) => {
-    // const isExpanded = expandedCategories.includes(category.id);
-    const isExpanded = openCategory === category.id;
+    const isExpanded = openCategories.includes(category.id);
+ 
     console.log(isExpanded);
     // console.log(category);
-
     return (
       <div key={category.id}>
         {/* cart design */}
         <div
-          className={`category-header cursor-pointer ${isExpanded ? 'active' : ''} flex items-center border-b-2 border-yellow-500 my-1`}
-          
-        onClick={() => toggleCategory(category.id)}
+          className={`category-header cursor-pointer ${
+            isExpanded ? "active" : ""
+          } flex items-center border-b-2 border-yellow-500 my-1`}
+          onClick={() => toggleCategory(category.id)}
         >
-       <div className="w-10 mx-3">
-        <img
-          src={`https://backend.bppshop.com.bd/storage/category/${category.icon}`}
-          alt=""
-        />
-      </div>
-      <div className="">{category.name}</div>
+          <div className="w-10 mx-3">
+            <img
+              src={`https://backend.bppshop.com.bd/storage/category/${category.icon}`}
+              alt=""
+            />
+          </div>
+          <div className="">{category.name}</div>
         </div>
-
 
         {/* dropdown functionality */}
         {isExpanded && category.childes && category.childes.length > 0 && (
           <ul className="subcategory-list">
             {category.childes.map((subcategory) => (
-              <li onClick={()=>console.log(subcategory.name)} key={subcategory.id} className="ml-10 border-l-2 border-yellow-300 pl-3">
-                {subcategory.name}
-                {
-                    subcategory.childes.map((subCate)=><RenderSubCategory key={subCate.id} subCate={subCate}/>)
-                }
+              <li
+         key={subcategory.id}
+                className="ml-10 border-l-2 border-yellow-300 pl-3"
+              >
+                {/* {subcategory.name} */}
+                {/* {subcategory.childes.map((subCate) =>
+                  console.log(subCate)
+                )} */}
+                {renderCategory(subcategory)}
               </li>
             ))}
           </ul>
@@ -67,7 +74,7 @@ const SideDropdownMenu = ({categories}) => {
       {categories.map((category) => renderCategory(category))}
     </div>
   );
-};
+  }
 
 
 export default SideDropdownMenu;
